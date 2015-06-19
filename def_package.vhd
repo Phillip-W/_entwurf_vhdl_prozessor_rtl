e9605c08bc0efb3da06733abe48110a984952d56
Phillip-W <Phillip-W@users.noreply.github.com>
2015-06-19 17:02:51 +0200

    Update and rename def_pac.vhd to def_package.vhd

---
+++ def_package.vhd
@@ -0,0 +1,10 @@
+PACKAGE def_package IS
+
+  CONSTANT data_width : POSITIVE := 12; --1.1.1
+	CONSTANT address_width : POSITIVE := 12; --1.1.2
+	CONSTANT opcode_width : POSITIVE := 6; --3.1.1
+
+	CONSTANT reg_addr_width : POSITIVE := 2; --2.1.2
+	CONSTANT addr_width : POSITIVE := 12; -- nicht Aendern
+	
+END def_package;
