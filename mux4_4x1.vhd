07277b5d808eea7baa78491a6dee1ebc56812740
flomaurer <flomaurer@users.noreply.github.com>
2015-06-23 12:42:43 +0200

    Update mux4_4x1.vhd

--- mux4_4x1.vhd
+++ mux4_4x1.vhd
@@ -13,14 +13,18 @@ begin
               d_in_a when others;
 end RTL;
 
-architecture RTL_clk of mux4_4x1 is	-- als Process, falls man es takten muss
+Entity muxN_4x1 is
+generic (N: natural);
+  port (  select_input                    :in bit_vector  (1 downto 0);
+          d_in_a, d_in_b, d_in_c, d_in_d  :in bit_vector  (N-1 downto 0);
+          d_out                           :out bit_vector (N-1 downto 0));
+end muxN_4x1;
+
+architecture RTL of muxN_4x1 is	
 begin
-	process (d_in_a, d_in_b, d_in_c, d_in_d)
-	begin
-		if select_input 	= "11" then d_out <= d_in_d;
-			elsif select_input 	= "10" then d_out <= d_in_c;
-			elsif select_input	= "01" then d_out <= d_in_b;
-			else				    d_out <= d_in_a;
-		end if;
-	end process;
-end RTL_clk;
+  with select_input select
+    d_out <=  d_in_d when "11",
+              d_in_c when "10",
+              d_in_b when "01",
+              d_in_a when others;
+end RTL;
