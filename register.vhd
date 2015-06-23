ac0034d45c7b54a17459b7b3ce8f6158c1468cf0
flomaurer <flomaurer@users.noreply.github.com>
2015-06-23 12:45:02 +0200

    Update register.vhd

--- register.vhd
+++ register.vhd
@@ -1,7 +1,35 @@
 
 
 use work.def_package.all;
+-- FLo Maurer
+entity reg is -- variable Datenwortbreite (Grundlage: Patrick Ulrich) 
+	generic (N: natural);
+	port(
+		D_IN : in bit_vector (N-1 downto 0);
+		RST  : in bit;
+		ENABLE: in bit;
+		CLK  : in bit;
+		Q_OUT: out bit_vector (N-1 downto 0)
+	);
+end reg;
 
+architecture RTL of reg is
+begin 
+	process (RST, CLK)
+	begin
+		if RST = '0' then
+		--Reset Action--
+		Q_OUT <= (others => '0');
+		elsif CLK = '1' and CLK'event then
+		--Action for operation modes
+		if ENABLE = '1' then
+			Q_OUT <= D_IN;
+		end if;
+	end if;
+	end process;
+end RTL;
+
+-- Patrick Ulrich
 entity D_REG12RE is
 	port(
 		D_IN : in bit_vector (data_width-1 downto 0);
