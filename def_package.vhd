72c1b5de83a5f5c53959a07a78bced1bcdd2bf8f
flomaurer <flomaurer@users.noreply.github.com>
2015-06-23 14:01:27 +0200

    Update def_package.vhd

--- def_package.vhd
+++ def_package.vhd
@@ -3,6 +3,7 @@ PACKAGE def_package IS
 	CONSTANT data_width : POSITIVE := 12; --1.1.1
 	CONSTANT addr_width : POSITIVE := 12; --1.1.2
 	CONSTANT opcode_width : POSITIVE := 6; --3.1.1
+	constant pc_width: Positiv:= addr_width; -- 2.1.3.1
 
 	CONSTANT reg_addr_width : POSITIVE := 2; --2.1.2
 	CONSTANT flag_width : POSITIVE := 4;	-- Flag Vector: (Z,C,N,O)
