34c6e9c31bf654c6b0c64b6a141aac1f43fc7cbd
flomaurer <flomaurer@users.noreply.github.com>
2015-06-23 13:09:45 +0200

    fertig mit Flags

--- shifter.vhd
+++ shifter.vhd
@@ -1,8 +1,9 @@
 USE work.def_package.ALL;
-
+-- geschrieben Flo Maurer (auf Basis des Verhaltensmodels von Lei)
 Entity shifter Is
   Port (OPCode: in opcode_type;
         Flags: in flag_type;
+	Flagsout: out flag_type;
         OP1, OP2: in data_type;
         result: out data_type);
 END ENTITY;
@@ -12,48 +13,42 @@ ARCHITECTURE shift of shifter IS
   BEGIN
     Process(OPCode)
       Begin
-        if OPCode = code_sll then     -- muss mir die einzelnen shifts noch anschauen
-          -- if OP1(OP1'left) = '1' then
-            -- Flags(2)<= True;
-          -- else
-            -- Flags(2)<= FALSE;
-          -- end if;
-          -- if (OP1(OP1'left)='1') xor (OP1(OP1'left-1)='1') then
-            -- Flags(0)<= True;
-          -- else
-            -- Flags(0)<= False;
-          -- end if;
+        if OPCode = code_sll then     
+           Flagsout(2)<=OP1(OP1'left);
+           if (OP1(OP1'left)='1') xor (OP1(OP1'left-1)='1') then
+             Flagsout(0)<= '1';
+           else
+             Flagsout(0)<= '0';
+           end if;
           result(result'left downto 1) <= OP1(OP1'left-1 downto 0);
           result(result'right) <= '0';
         elsif OPCode = code_srl then
-          -- if OP1(OP1'right) = '1' then
-            -- Flags(2)<= True;
-          -- else
-            -- Flags(2)<= False;
-          -- end if;
+            Flagsout(2)<=OP1(OP1'right);Flagsout(0)<= '0';
             result(result'left) <= '0';
             result(result'left-1 downto 0) <= OP1(OP1'left downto 1);
         elsif OPCode = code_sra then
-          -- if OP1(OP1'right) = '1' then
-            -- Flags(2)<= True;
-          -- else
-            -- Flags(2)<= False;
-          -- end if;
+          Flagsout(2)<=OP1(OP1'right); Flagsout(0)<= '0';
           result(result'left) <=OP1(OP1'left);
           result(result'left-1 downto 0) <= OP1(OP1'left downto 1);
         elsif OPCode = code_rol OR OPCode = code_rolc then
+	  Flagsout(0)<= '0';
           if Flags(2)='1' then
+	    Flagsout(2)<=OP1(OP1'left);
             result(result'left downto 1) <= OP1(OP1'left-1 downto 0);
             result(result'right) <= '1' ;
           else
+	    Flagsout(2)<='0';
             result(result'left) <= OP1(OP1'right);
             result(result'left-1 downto 0) <= OP1(OP1'left downto 1);
           end if;
         elsif OPCODE = code_ror OR OPCode = code_RORC then
+	  Flagsout(0)<= '0';
           if flags(2)='1' then
+	    Flagsout(2)<=OP1(OP1'right);
             result(result'left) <= '1';
             result(result'left-1 downto 0) <= OP1(OP1'left downto 1);
           else
+	    Flagsout(2)<='0';
             result(result'right) <= OP1(OP1'left);
             result(result'left downto 1) <= OP1(OP1'left-1 downto 0);
           end if;
