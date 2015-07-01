66dcd68047c9c23da5481568c5f250e404e8a3ae
Phillip-W <Phillip-W@users.noreply.github.com>
2015-07-01 20:19:25 +0200

    Update FC.vhd
    fc_sel included
    pc -> d_in

--- FC.vhd
+++ FC.vhd
@@ -2,9 +2,10 @@ USE work.def_package.all;
 
 ENTITY flag_check IS
   PORT(
+    SIGNAL fc_sel: IN bit;
     SIGNAL flag_in: IN flag_type;
     SIGNAL alu_res: IN data_type;
-    SIGNAL pc: IN addr_type;
+    SIGNAL d_in: IN data_type;
     SIGNAL flag_out: OUT flag_type
     
   );
@@ -13,11 +14,17 @@ END flag_check;
 ARCHITECTURE behav OF flag_check IS
   CONSTANT zero: data_type := (OTHERS => '0'); 
 BEGIN
-  check: PROCESS(alu_res, pc)
+  check: PROCESS(alu_res, d_in, fc_sel)
   BEGIN
     flag_out <= flag_in(3 downto 1) & '0';
-    IF alu_res = zero OR pc = zero THEN
-      flag_out(0) <= '1';
+    IF fc_sel = '0' THEN
+      IF alu_res = zero THEN
+        flag_out(0) <= '1';
+      END IF;
+    ELSE
+      IF d_in = zero THEN
+        flag_out(0) <= '1';
+      END IF;
     END IF;
   END PROCESS check;
 END behav;
