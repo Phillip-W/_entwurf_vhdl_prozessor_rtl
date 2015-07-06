55a95ba9178215c4285a0c1676f7f3465c887ee7
flomaurer <flomaurer@users.noreply.github.com>
2015-07-06 10:39:44 +0200

    Update conf_system.vhd

--- conf_system.vhd
+++ conf_system.vhd
@@ -1,10 +1,14 @@
 Configuration conf of System is
   for behav
-    for mux_data: mux 
-      use entity work.mux12_4x1(RTL);
+    for mux_data: muxdata 
+      use entity work.muxdata(RTL);
     end for;
-    for mux_sig: mux
-      use entity work.mux12_4x1(RTL);
+    for mux_sig: muxsig
+      use entity work.muxsig(RTL);
     end for;
+    for UPC: CPU
+      use entity work.CPU(RTL);
+    END FOR;
   End for;
 End configuration;
+
